08a1908eefc7f759c80c9523b5b3b47000f5ef59
Wassim Rahali <wassimrahali40@gmail.com>
2024-08-12 17:10:07 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-
-
-function App() {
-
-  return (
-    
-    <>
+import { Component } from "react";
+export default class App extends Component {
+  myMethod(prenom){
+    return <span>{prenom}</span>
+  }
+  render() {
  
-    </>
-  )
+    const name = "Wassim"
+    const prenom = this.myMethod("Mohamed")
+    return (
+      <div>
+        <p>{name} {prenom}</p>
+      </div>
+    );
+  }
 }
-
-export default App
---
+++ src/Componnents/File.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class File extends Component {
+  render() {
+    return (
+      <div>File</div>
+    )
+  }
+}
