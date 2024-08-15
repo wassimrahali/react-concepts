7e0d0e591e071e27c39f4c62032d3df2c079a3c7
Wassim Rahali <wassimrahali40@gmail.com>
2024-08-15 15:03:44 +0100

    use Context

--- src/ComponnentA.tsx
+++ src/ComponnentA.tsx
@@ -4,12 +4,19 @@ export const userContext = createContext();
 
 function ComponnentA() {
     const [user,setUser] = useState('Wassim')
+
+  const update =()=>{
+    setUser('Rahali')
+  }
+
   return (
     <div className='box'>ComponnentA
     <h1>Hello {user} from the top</h1>
     <userContext.Provider value={user} >
     <ComponnentB />
+    
     </userContext.Provider>
+    <button onClick={update} >click here </button>
     </div>
   )
 }
