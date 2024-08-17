0dd9a716f922827678cbe81cfc8d8106b9b7a1dc
Wassim Rahali <wassimrahali40@gmail.com>
2024-08-18 00:42:57 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import Bg from "./Bg"
-import Footer from "./Footer"
-import Navbar from "./Navbar"
-import ReducerTaskManager from "./ReducerTaskManager"
-
 
 function App() {
 
   return (
   <>
- {/* <Navbar />
-  <ReducerTaskManager />
-  <Footer />  */}
+
   <Bg/>
   </>
   )
