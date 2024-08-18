372061069ae065c24237b440163bafd543102240
Wassim Rahali <wassimrahali40@gmail.com>
2024-08-18 02:05:07 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import Bg from "./Bg"
+import React, { useState, useEffect } from "react";
+import Bg from "./Bg";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, []); 
 
   return (
-  <>
-
-  <Bg/>
-  </>
-  )
+    <div>
+      {isLoading ? (
+        <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <progress className="progress w-56"></progress>
+        </div>
+      ) : (
+        <Bg />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
